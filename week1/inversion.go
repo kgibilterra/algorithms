8f1e1c1ffe91d5a24610f11f1e83f9c65153d88a
Kaylyn Gibilterra <kgibilterra@gmail.com>
2016-07-19 17:59:34 -0400

    first working version

--- week1/inversion.go
+++ week1/inversion.go
@@ -25,48 +25,6 @@ func mergesort(a []int) []int {
 	return merge3(len(a), mergesort(a[0:len(a)/2]), mergesort(a[len(a)/2:]))
 }
 
-func merge(b []int, c []int) []int {
-	fmt.Println("B: ", b, "C: ", c)
-
-	r = []int{}
-	for i := 0; i < len(b); i++ {
-		fmt.Println("B: ", b[i], "C: ", c[0])
-		if b[i] > c[0] {
-			r = append(r, c[0])
-			c = append(c[:0], c[1:]...)
-			fmt.Println("DELETE C? ", c)
-		} else {
-			r = append(r, b[i])
-		}
-		fmt.Println("R: ", r)
-	}
-	fmt.Println(r)
-	return r
-}
-
-func merge2(b []int, c []int) []int {
-	fmt.Println("B: ", b, "C: ", c)
-
-	r = []int{}
-	for i := 0; i < len(b); i++ {
-		for j := 0; j < len(c); j++ {
-			fmt.Println("B: ", b[i], "C: ", c[0])
-			if b[i] > c[j] {
-				r = append(r, c[j])
-			} else {
-				r = append(r, b[i])
-				fmt.Println("START C: ", c)
-				c = append(c[:j], c[j:]...)
-				fmt.Println("END C: ", c)
-				break
-			}
-
-		}
-	}
-	fmt.Println(r)
-	return r
-}
-
 func merge3(n int, b []int, c []int) []int {
 	var a []int
 	i := 0
