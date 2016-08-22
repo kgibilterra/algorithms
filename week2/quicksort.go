b3a574438aff45b102ef2e170d6edb3aafbc6330
Kaylyn Gibilterra <kgibilterra@gmail.com>
2016-08-21 23:13:32 -0400

    working except comparisons for median...

--- week2/quicksort.go
+++ week2/quicksort.go
@@ -25,35 +25,56 @@ func main() {
 		x, _ := strconv.Atoi(scanner.Text())
 		data = append(data, x)
 	}
-	fmt.Println("length of data:", len(data), "first 10 elements:", data[:10])
-	fmt.Println("Starting Array:", data)
+	fmt.Println("Starting Array:", data[:10])
 
-	A := partitionArray(data)
-	fmt.Println("Final Array:", A)
+	A := partitionArray(data, 0, len(data))
+	fmt.Println("Final Array:", A[:10])
 	fmt.Println("comparisons:", comparisons)
 
 }
 
-func partitionArray(A []int) []int {
-	comparisons += len(A) - 1
+func partitionArray(A []int, l int, r int) []int {
+	//comparisons += len(A) - 1
 	if len(A) < 2 {
 		return A
 	}
-	pivot := selectPivot(A)
+
+	// don't use anything when pivot is the first element
+	//p := A[l]
+
+	// use this when pivot is the last element
+	A[r-1], A[l] = A[l], A[r-1]
+	p := A[l]
+
+	// "median" pivot
+	// median := len(A) / 2
+	// if len(A)%2 == 0 {
+	// 	median--
+	// }
+	// pivots := []int{A[l], A[r-1], A[median]}
+	// p := pivots[0]
+	// if pivots[1] < pivots[2] && pivots[1] < pivots[0] {
+	// 	p = pivots[1]
+	// 	A[r-1], A[l] = A[l], A[r-1]
+	// } else if pivots[2] < pivots[1] && pivots[2] < pivots[0] {
+	// 	p = pivots[2]
+	// 	A[median], A[l] = A[l], A[median]
+	// }
 
 	// partition Array
-	i := pivot + 1
-	for j := pivot + 1; j < len(A); j++ {
-		if A[j] < A[pivot] {
+	i := l + 1
+	for j := l + 1; j < r; j++ {
+		comparisons++
+		if A[j] < p {
 			A[i], A[j] = A[j], A[i]
 			i++
 		}
 	}
-	A[i-1], A[pivot] = A[pivot], A[i-1]
+	A[l], A[i-1] = A[i-1], A[l]
 
 	// call partition on left and right sides of array
-	partitionArray(A[:i-1])
-	partitionArray(A[i:])
+	partitionArray(A[:i-1], 0, len(A[:i-1]))
+	partitionArray(A[i:], 0, len(A[i:]))
 
 	return A
 }
